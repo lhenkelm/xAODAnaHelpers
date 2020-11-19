b498c43e224b4f2b02e6d288ab03b1022a578841
Lars Henkelmann <lars.henkelmann@cern.ch>
2020-11-19 17:49:33 +0000

    additional JSS branches in FatJetContainer

--- xAODAnaHelpers/FatJet.h
+++ xAODAnaHelpers/FatJet.h
@@ -44,6 +44,10 @@ namespace xAH {
       float  ECF3;
       float  C2;
       float  D2;
+      float  N2;
+      float  M2;
+      float  D2_1_2; // D_2^(1,2) of arxiv:1609.07483 [hep-ph]
+      float  D2_1p7; // D_2^(\beta=1.7) as in arxiv:1409.6298 [hep-ph]
       float  NTrimSubjets;
       int    NClusters;
       int    nTracks;
--- xAODAnaHelpers/FatJetContainer.h
+++ xAODAnaHelpers/FatJetContainer.h
@@ -86,6 +86,10 @@ namespace xAH {
       std::vector<float> *m_ECF3;
       std::vector<float> *m_C2;
       std::vector<float> *m_D2;
+      std::vector<float> *m_N2;
+      std::vector<float> *m_M2;
+      std::vector<float> *m_D2_1_2; // D_2^(1,2) of arxiv:1609.07483 [hep-ph]
+      std::vector<float> *m_D2_1p7; // D_2^(\beta=1.7) as in arxiv:1409.6298 [hep-ph]
       std::vector<float> *m_NTrimSubjets;
       std::vector<int>   *m_NClusters;
       std::vector<int>   *m_nTracks;
